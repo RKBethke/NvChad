f1d39de3e11c119990373e18932f00470b8227f7
ryanb-dev <ryanb.404dev@gmail.com>
2021-12-24 23:09:20 -0600

    Delete cruft.

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,9 +3,8 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
-   -- lspservers with default config
-
-   local servers = { "rust_analyzer", "clangd", "gopls" }
+   -- lspservers with default config.
+   local servers = { "rust_analyzer", "clangd" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
@@ -17,13 +16,4 @@ M.setup_lsp = function(attach, capabilities)
    end
  end
 
-vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
-})
-
 return M
-
